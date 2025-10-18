d3da0c07f7997d808cbd3c1c3932d72c67400d2a
mrc-p <marcos.paulo.spires@gmail.com>
2025-10-18 19:31:39 -0300

    Lista de projetos criada

---
+++ app/src/main/java/data/Project.kt
@@ -0,0 +1,9 @@
+package data
+
+val mockProjects = listOf(
+    Project(1, "App de Portfólio", "Este mesmo aplicativo.", "Kotlin"),
+    Project(2, "API de Biblioteca Online", "API Restful de Biblioteca Online", "Python/Django"),
+    Project(3, "Website", "Site de catálogo de filmes", "HTML+CSS+JavaScript"),
+    Project(4, "API DJ System", "API simples para testar token JWT", "Python/Flask"),
+    Project(5, "App de Lista de Tarefas", "Um simples app de To-Do.", "Kotlin")
+)
